9ed0c8daa9d8569b63730a7fd07f81bd08dea6c8
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2021-12-03 20:53:03 -0300

    instalación de popper en plantilla frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core'
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
